c96bf2ede8693fb328b18bc32e38f9427b4369ee
adamberman <adamberman13@gmail.com>
2015-06-28 23:08:58 -0700

    better isGoodEnough function

--- functional_programming/.worksheet/src/week1.session.scala
+++ functional_programming/.worksheet/src/week1.session.scala
@@ -5,10 +5,10 @@ object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport.
 	
 	def sqrtIter(guess: Double, x: Double): Double =
 		if (isGoodEnough(guess, x)) guess
-		else sqrtIter(improve(guess, x), x);System.out.println("""sqrtIter: (guess: Double, x: Double)Double""");$skip(82); 
+		else sqrtIter(improve(guess, x), x);System.out.println("""sqrtIter: (guess: Double, x: Double)Double""");$skip(86); 
 		
 	def isGoodEnough(guess: Double, x: Double) =
-		abs(guess * guess - x) < 0.001;System.out.println("""isGoodEnough: (guess: Double, x: Double)Boolean""");$skip(70); 
+		abs(guess * guess - x) / x < 0.001;System.out.println("""isGoodEnough: (guess: Double, x: Double)Boolean""");$skip(70); 
 		
 	def improve(guess: Double, x: Double) =
 		(guess + x / guess) / 2;System.out.println("""improve: (guess: Double, x: Double)Double""");$skip(43); 
@@ -16,5 +16,7 @@ object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport.
 	def sqrt(x: Double) = sqrtIter(1.0, x);System.out.println("""sqrt: (x: Double)Double""");$skip(11); val res$0 = 
 	
 	sqrt(2);System.out.println("""res0: Double = """ + $show(res$0));$skip(9); val res$1 = 
-	sqrt(4);System.out.println("""res1: Double = """ + $show(res$1))}
+	sqrt(4);System.out.println("""res1: Double = """ + $show(res$1));$skip(12); val res$2 = 
+	sqrt(1e-6);System.out.println("""res2: Double = """ + $show(res$2));$skip(12); val res$3 = 
+	sqrt(1e60);System.out.println("""res3: Double = """ + $show(res$3))}
 }
