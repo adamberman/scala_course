88466cc2277734c9147ab1cf72c0e95205a6a0d5
adamberman <adamberman13@gmail.com>
2015-06-29 23:13:49 -0700

    get rid of unneeded arguments passed around

--- functional_programming/.worksheet/src/week1.session.scala
+++ functional_programming/.worksheet/src/week1.session.scala
@@ -1,21 +1,21 @@
 package week1
 
-object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(432); 
+object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(387); 
   def sqrt(x: Double) = {
 
     def abs(x: Double) = if (x < 0) -x else x
 
-    def sqrtIter(guess: Double, x: Double): Double =
-      if (isGoodEnough(guess, x)) guess
-      else sqrtIter(improve(guess, x), x)
+    def sqrtIter(guess: Double): Double =
+      if (isGoodEnough(guess)) guess
+      else sqrtIter(improve(guess))
 
-    def isGoodEnough(guess: Double, x: Double) =
+    def isGoodEnough(guess: Double) =
       abs(guess * guess - x) / x < 0.001
 
-    def improve(guess: Double, x: Double) =
+    def improve(guess: Double) =
       (guess + x / guess) / 2
 
-    sqrtIter(1.0, x)
+    sqrtIter(1.0)
   };System.out.println("""sqrt: (x: Double)Double""");$skip(10); val res$0 = 
   sqrt(2);System.out.println("""res0: Double = """ + $show(res$0));$skip(10); val res$1 = 
   sqrt(4);System.out.println("""res1: Double = """ + $show(res$1));$skip(13); val res$2 = 
