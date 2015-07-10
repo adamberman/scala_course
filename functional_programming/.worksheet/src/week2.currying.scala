02dd6218c130a5d060686eeb07c065ec38027146
adamberman <adamberman13@gmail.com>
2015-07-10 01:07:52 -0700

    turn product into factorial

--- functional_programming/.worksheet/src/week2.currying.scala
+++ functional_programming/.worksheet/src/week2.currying.scala
@@ -5,5 +5,8 @@ object currying {;import org.scalaide.worksheet.runtime.library.WorksheetSupport
   	if (a>b) 1
   	else f(a) * product(f)(a + 1, b)
   };System.out.println("""product: (f: Int => Int)(a: Int, b: Int)Int""");$skip(28); val res$0 = 
-  product(x => x * x)(3, 4);System.out.println("""res0: Int = """ + $show(res$0))}
+  product(x => x * x)(3, 4);System.out.println("""res0: Int = """ + $show(res$0));$skip(51); 
+  
+  def factorial(n: Int) = product(x => x)(1, n);System.out.println("""factorial: (n: Int)Int""");$skip(15); val res$1 = 
+  factorial(5);System.out.println("""res1: Int = """ + $show(res$1))}
 }
